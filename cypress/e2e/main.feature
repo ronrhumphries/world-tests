8f7f2b6de8075eca14ee90ff9fb0f1bdb8f73b1f
Humphries <ac60815@usa-x7031299.lan>
2022-09-20 23:10:26 -0500

    Refined test

--- cypress/e2e/main.feature
+++ cypress/e2e/main.feature
@@ -3,4 +3,4 @@ Feature: World main functionality
   Scenario: User starts a world session by opening the world URL up in their browser
     Given a user has the world URL
     When the user navigates to the world URL
-    Then the user sees the page titled World
+    Then the user sees the "command>" prompt
--- cypress/e2e/main/main.js
+++ cypress/e2e/main/main.js
@@ -3,13 +3,13 @@ import { Given, When, Then } from "@badeball/cypress-cucumber-preprocessor";
 var url = ''
 
 Given('a user has the world URL', () => {
-  url = "http://localhost"
+  url = "http://localhost:3000"
 })
 
 Given('the user navigates to the world URL', () => {
   cy.visit(url)
 })
 
-Then('the user sees the page titled World', () => {
-  cy.title().should('eq', 'World')
+Then('the user sees the "command>" prompt', () => {
+  cy.get('#prompt').should('contain.text', 'command>')
 })
